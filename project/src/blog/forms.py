21706431b06528422fdeac6a4e0ae93f17eb5535
SubestaRomany <139684831+SubestaRomany@users.noreply.github.com>
2023-09-16 21:43:46 -0400

    Delete project directory

--- project/src/blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Post
-class PostCreateForm(forms.ModelForm):
-    title = forms.CharField(label='Title')
-    content = forms.CharField(label='Content', widget=forms.Textarea)
-
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
